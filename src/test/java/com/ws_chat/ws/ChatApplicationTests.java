5115e7cee4e57a66efea9e1340486878a6eafd11
Emalyanov535 <nouctanch777093@gmail.com>
2024-02-16 14:18:44 +0400

    init project

---
+++ src/test/java/com/ws_chat/ws/ChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ws_chat.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
